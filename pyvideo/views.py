2fc52c72e3d3039554f0592cebf81c3bd6f70d5e
Michał Podeszwa <michal.podeszwa@gmail.com>
2014-10-02 19:32:34 +0200

    Added application authoization

--- pyvideo/views.py
+++ pyvideo/views.py
@@ -1,6 +1,14 @@
 from . import app
+from .auth import requires_auth
 
 
 @app.route("/")
 def index():
     return "Hello world"
+
+
+@app.before_request
+@requires_auth
+def require_auth():
+    """This function is called only so that requires_auth is called"""
+    pass
